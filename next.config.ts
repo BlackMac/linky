74c95ef2930d04e03053f6c9b4bb365ec176ab55
Stefan Lange-Hegermann <lange-hegermann@sipgate.de>
2025-05-05 15:19:02 +0200

    better config handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  reactStrictMode: true,
+  // Allow serving both local and remote images
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    dangerouslyAllowSVG: true, // Allow SVG files
+    contentDispositionType: 'attachment', // Handle downloads properly
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // Secure SVG handling
+  },
 };
 
-export default nextConfig;
+export default config;
